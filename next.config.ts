95b50cfe53f8ba830b0a1e250c3b8e39d72af4f3
Hades <gbalyukw@gmail.com>
2025-06-15 15:48:31 -0400

    Initial Setups

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,  // Extra Checks in Development
+  images: {
+    domains: [],
+  },
+  env: {},
 };
 
 export default nextConfig;
